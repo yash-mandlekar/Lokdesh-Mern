ec872862f62b5d863f6b021c0d82471d927822c5
yash-mandlekar <91611265+yash-mandlekar@users.noreply.github.com>
2022-12-19 13:34:08 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,29 @@
+require("dotenv").config({ path: "./config/.env" });
+const express = require("express");
+const mongoose = require('mongoose');
+const app = express();
+
+// Connect to MongoDB Atlas
+const cookieParser = require("cookie-parser");
+const bodyParser = require("body-parser");
+const errormiddleware = require("./middleware/error");
+const path = require("path");
+require("./config/database").databaseconnection();
+const userRouter = require("./routes/userRoutes");
+const adminRouter = require("./routes/adminRoutes");
+app.use(require("cors")({ credentials: true, origin: process.env.FRONTEND_URL }));
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use("/user/", userRouter);
+app.use("/", adminRouter);
+app.use(errormiddleware);
+
+app.listen(
+  process.env.PORT,
+  console.log(`Server started on port ${process.env.PORT}`)
+);
